70619d1e38dde5acc443df919dee0cc59ee17601
Edwin <emadrig1@uci.edu>
2022-12-06 15:29:17 -0800

    Feature 6 complete

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,7 @@ from .models import Project
 # Create your views here.
 
 
-def project_list(request):
+def list_projects(request):
     project_list = Project.objects.all()
     context = {"project_list": project_list}
 
